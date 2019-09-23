38d23db0a749dfab9494aec687d6d50b9c246f9d
Ramarao <sampathirao.ramarao@alticeusa.com>
2019-09-23 13:58:19 +0530

    complete with Project

---
+++ NYtimes/NYtimes/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  NYtimes
+//
+//  Created by Ramarao Sampathirao on 9/23/19.
+//  Copyright © 2019 Ramarao Sampathirao. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+
+}
